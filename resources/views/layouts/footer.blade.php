07916321e29bda20179b8719be785bfeafae3987
noswel <noswelvr@gmail.com>
2023-07-25 12:11:13 +0700

    Update footer.blade.php
    changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer-left">
     Copyright &copy; {{ now()->year }} <div class="bullet"></div> Design By <a
         href="https://mustika.in/">Mustika Putri
-        </a> Laravel Code By <a href="https://putraprima.id">WaktuSaku Team</a>
+        </a> Laravel Code By <a href="https://waktusaku.id">WaktuSaku Team</a>
 </div>
 <div class="footer-right">
     2.3.0
