173c6a8c53aa51ac16ea71bda5fe1de16145380d
vennymeida <vnymeida@gmail.com>
2023-07-25 10:44:07 +0700

    starter

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="footer-left">
+    Copyright &copy; {{ now()->year }} <div class="bullet"></div> Design By <a
+        href="https://nauval.in/">Mustika Putri
+        </a> Laravel Code By <a href="https://putraprima.id">WaktuSaku Team</a>
+</div>
+<div class="footer-right">
+    2.3.0
+</div>
