592d4f09a69e4b55836546a1ad463b8885ea80f2
vennymeida <vnymeida@gmail.com>
2023-08-13 00:28:08 +0700

    revisi landing page (frontend)

--- resources/views/landing-page/navigation.blade.php
+++ resources/views/landing-page/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-primary sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-primary-nav sticky-top">
     <div class="container">
         <a class="navbar-brand d-flex align-items-center" href="/">
-            <img src="{{ asset('assets/img/landing-page/logo.png') }}" style="height: 35px; alt="">
+            <img src="{{ asset('assets/img/landing-page/logo-2.png') }}" style="height: 35px; alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,31 +10,31 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active mr-4">
-                    <a class="nav-link text-white" href="{{ url('/') }}">Home</a>
+                    <a class="nav-link text-primary" href="{{ url('/') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white mr-4" href="{{ url('/all-jobs') }}">Lowongan Pekerjaan</a>
+                    <a class="nav-link text-primary mr-4" href="{{ url('/all-jobs') }}">Lowongan Pekerjaan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white mr-4" href="{{ url('/contact') }}">Contact Us</a>
+                    <a class="nav-link text-primary mr-4" href="{{ url('/contact') }}">Contact Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ url('/list-about') }}">About Us</a>
+                    <a class="nav-link text-primary" href="{{ url('/list-about') }}">About Us</a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 @if (!auth()->user())
                     <li class="nav-item">
-                        <a class="nav-link btn btn-primary text-white" href="{{ route('login') }}">Login</a>
+                        <a class="nav-link btn btn-outline-primary text-primary" href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link btn btn-outline-primary" href="{{ route('register') }}">Sign Up</a>
+                        <a class="nav-link btn btn-outline-warning text-white" href="{{ route('register') }}">Daftar</a>
                     </li>
                 @else
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button class="nav-link btn btn-outline-primary text-white" type="submit">Logout</button>
+                            <button class="nav-link btn btn-outline-primary text-primary" type="submit">Logout</button>
                         </form>
                     </li>
                 @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,119 +2,109 @@
 @section('main')
     <main class="bg-light">
         <section>
-            <div class="col-md-10 bg-white mx-auto my-5 p-5 rounded">
-                <h1>Selamat datang di <span class="text-primary">WaktuSaku</span></h1>
-                <p>Yuk daftar sekarang! Dapatakan tawaran pekerjaan sesuai minat bakat Anda</p>
-            </div>
-        </section>
-
-        <section>
-            <div class="card-primary col-md-10 bg-white mx-auto my-4 p-5 rounded">
-                <h2>Apa itu <span class="text-primary">WaktuSaku </span><span>?</span></h2>
+            <div class="col-md-12">
                 <div class="d-flex justify-content-around align-items-center">
-                    <div class="col-md-4">
-                        <img class="img-fluid" src="{{ asset('assets/img/landing-page/image2.png') }}" alt="">
-                    </div>
                     <div class="col-md-6">
-                        <h6 class="text-justify">
-                            Platform yang dirancang khusus untuk Mahasiswa Malang Raya yang mencari peluang kerja . Di
-                            WaktuSaku, kamu memahami betapa berharga waktu Anda sebagai mahasiswa yang sibuk. Oleh karena
-                            itu, kamu menawarkan berbagai peluang pekerjaan yang fleksibel dan relevan dengan
-                            minat bakat Anda.
-                        </h6>
+                        <h1>Temukan Peluang Kerja di <span class="text-primary">Waktu</span><span class="text-warning">Saku</span>
+                        Sesuai Minat Bakat Anda!</h1>
+                        <p>Platform yang dirancang untuk memudahkan mencari peluang kerja yang sesuai dengan kebutuhan Anda khusus di daerah Malang Raya.</p>
+                        <div class="form-row">
+                            <div class="form-group col-md-10">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Ketik Posisi Pekerjaan" value="{{ app('request')->input('nama') }}">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <button id="search-button" class="btn btn-primary mr-1" type="submit">Cari</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img class="img-fluid" src="{{ asset('assets/img/landing-page/image-3.png') }}" alt="">
                     </div>
                 </div>
             </div>
         </section>
 
         <section>
-            <div class="container">
+            <div class="bg-secondary my-5 mx-auto" style="height: 100%;">
                 <div class="row">
                     <div class="col-md-10 my-5 mx-auto">
-                        <h2 class="text-center">Mengapa harus di <span class="text-primary">WaktuSaku </span><span>?</span>
+                        <h2 class="text-center">Mengapa harus di <span class="text-white">WaktuSaku </span><span class="text-warning">Saku </span><span>?</span>
                         </h2>
                     </div>
                 </div>
-                <div class="row justify-content-center mb-3">
-                    <div class="col-md-5">
-                        <div class="card d-flex align-items-center" style="height: 100%; border: none;">
-                            <div class="row no-gutters">
-                                <div class="col-md-5 d-flex justify-content-center align-items-center"
-                                    style="padding-top: 50px;">
-                                    <i class="fas fa-user-check text-primary fa-3x" style="margin-bottom: 150px;"></i>
+                <div class="row col-md-12 my-5 mx-auto">
+                    <div class="col-md-3">
+                        <div class="card" style="border: none;">
+                            <div class="card-body">
+                                <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 50px; margin-top:50px">
+                                    <i class="fas fa-user-check text-primary fa-3x"></i>
                                 </div>
-                                <div class="col-md-7 bg-primary text-white rounded">
-                                    <div class="card-body">
-                                        <h5 class="card-title font-weight-bold">Peluang Pekerjaan yang Sesuai</h5>
-                                        <p class="card-text text-justify">
-                                            Temukan lowongan kerja yang
-                                            relevan dengan minat bakat Anda. Mulai dari perusahaan teknologi,
-                                            media kreatif, hingga startup yang sedang berkembang pesat.
-                                        </p>
-                                    </div>
-                                </div>
+                                <p class="card-title text-center font-weight-bold h5">Peluang Pekerjaan Sesuai</p>
+                                <p class="card-text text-justify">
+                                    Temukan lowongan kerja yang relevan dengan minat bakat Anda. Mulai dari perusahaan teknologi,
+                                    media kreatif, hingga startup yang sedang berkembang pesat.
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-5">
-                        <div class="card d-flex align-items-center" style="height: 100%; border: none;">
-                            <div class="row no-gutters">
-                                <div class="col-md-5 d-flex justify-content-center align-items-center"
-                                    style="padding-top: 50px;">
-                                    <i class="fas fa-file-alt text-primary fa-3x" style="margin-bottom: 150px;"></i>
+                    <div class="col-md-3">
+                        <div class="card" style="border: none;">
+                            <div class="card-body">
+                                <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 50px; margin-top:50px">
+                                    <i class="fas fa-file-alt text-primary fa-3x"></i>
                                 </div>
-                                <div class="col-md-7 bg-primary text-white rounded">
-                                    <div class="card-body">
-                                        <h5 class="card-title font-weight-bold">Pengalaman Berharga</h5>
-                                        <p class="card-text text-justify">
-                                            Peluang kerja di WaktuSaku membantu Anda dalam mendapatkan pengalaman
-                                            kerja lebih awal. WaktuSaku akan memberikan keuntungan yang
-                                            signifikan dalam pasar kerja setelah lulus.
-                                        </p>
-                                    </div>
+                                <p class="card-title text-center font-weight-bold h5">Pengalaman Berharga</p>
+                                <p class="card-text text-justify">
+                                    Peluang kerja di WaktuSaku membantu Anda dalam mendapatkan pengalaman
+                                    kerja lebih awal. WaktuSaku akan memberikan keuntungan yang
+                                    signifikan dalam pasar kerja setelah lulus.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border: none;">
+                            <div class="card-body">
+                                <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 50px; margin-top:50px">
+                                    <i class="fas fa-history text-primary fa-3x"></i>
                                 </div>
+                                <p class="card-title text-center font-weight-bold h5">Waktu yang Fleksibel</p>
+                                <p class="card-text text-justify">
+                                    Kamu memahami prioritas utama Anda sebagai mahasiswa. Oleh karena itu, Anda
+                                    dapat menyesuaikan jadwal kerja Anda dengan jadwal kuliah dan kegiatan lainnya.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border: none;">
+                            <div class="card-body">
+                                <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 50px; margin-top:50px">
+                                    <i class="fas fa-percent text-primary fa-3x"></i>
+                                </div>
+                                <p class="card-title text-center font-weight-bold h5">Mendapatkan gaji bonus</p>
+                                <p class="card-text text-justify">
+                                    Selain mendapatkan peluang pekerjaan yang sesuai, Anda mendapatkan gaji bonus
+                                    dari masing - masing perusahaan yang Anda minati jika Anda pekerja aktif.
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-5">
-                        <div class="card d-flex align-items-center" style="height: 100%; border: none;">
-                            <div class="row no-gutters">
-                                <div class="col-md-5 d-flex justify-content-center align-items-center"
-                                    style="padding-top: 50px;">
-                                    <i class="fas fa-history text-primary fa-3x" style="margin-bottom: 150px;"></i>
-                                </div>
-                                <div class="col-md-7 bg-primary text-white rounded">
-                                    <div class="card-body">
-                                        <h5 class="card-title font-weight-bold">Waktu yang Fleksibel</h5>
-                                        <p class="card-text text-justify">
-                                            Kamu memahami prioritas utama Anda sebagai mahasiswa. Oleh karena itu, Anda
-                                            dapat menyesuaikan jadwal kerja Anda dengan jadwal kuliah dan kegiatan lainnya.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="col-md-12">
+                <div class="d-flex justify-content-around align-items-center">
+                    <div class="col-md-4">
+                        <img class="img-fluid" src="{{ asset('assets/img/landing-page/image-4.png') }}" alt="">
                     </div>
-                    <div class="col-md-5">
-                        <div class="card d-flex align-items-center" style="height: 100%; border: none;">
-                            <div class="row no-gutters">
-                                <div class="col-md-5 d-flex justify-content-center align-items-center"
-                                    style="padding-top: 50px;">
-                                    <i class="fas fa-percent text-primary fa-3x" style="margin-bottom: 150px;"></i>
-                                </div>
-                                <div class="col-md-7">
-                                    <div class="card-body bg-primary text-white rounded">
-                                        <h5 class="card-title font-weight-bold">Mendapatkan gaji bonus</h5>
-                                        <p class="card-text text-justify">
-                                            Selain mendapatkan peluang pekerjaan yang sesuai, Anda mendapatkan gaji bonus
-                                            dari masing - masing perusahaan yang Anda minati jika Anda pekerja aktif.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="col-md-6">
+                        <h1>Mau Tahu Status Lamaran Kamu Saat Ini Bagaimana ? <span class="text-primary">Bisa!</span></h1>
+                        <p>Lacak status terbaru lamaran kerjamu kapan saja.</p>
+                        <p>Yuk Buat Akun Sekarang juga!</p>
+                        <a id="register-button" class="btn btn-primary" href="{{ route('register') }}">Buat Akun Sekarang</a>
                     </div>
                 </div>
             </div>
