3c5d29b15b6eb1208bc45cd4d39284761ead1932
RosisHudaya <rosishudayaputra02@gmail.com>
2023-08-22 15:11:35 +0700

    setting fortify

--- config/fortify.php
+++ config/fortify.php
@@ -62,6 +62,8 @@ return [
     */
 
     'home' => RouteServiceProvider::HOME,
+    'home-perusahaan' => RouteServiceProvider::HOME_PERUSAHAAN,
+    'home-pelamar' => RouteServiceProvider::HOME_PELAMAR,
 
     /*
     |--------------------------------------------------------------------------
@@ -134,7 +136,7 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        Features::emailVerification(), 
+        Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
         Features::twoFactorAuthentication([
