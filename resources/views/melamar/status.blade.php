6a0cac4ac4702726cd424cd2d6749f2821a2572f
noswel <noswelvr@gmail.com>
2023-08-24 12:19:11 +0700

    search function not done yet

--- resources/views/melamar/status.blade.php
+++ resources/views/melamar/status.blade.php
@@ -43,17 +43,15 @@
         <!-- End: Search form -->
 
         <section>
-            
-            <div class="col-md-10 mt-5 mx-auto justify-content-center">
+            <div class="col-md-10 mt-2 mx-auto justify-content-center">
                 @include('layouts.alert')
-                <div class="card shadow-sm">
                     <div class="card-body">
                         @forelse($lamaran as $lamar)
                         <div class="col-12 col-sm-12 mb-4">
                             <div class="card h-100">
                                 <div class="card-body d-flex flex-column">
                                     <div class="media">
-                                        <div class="mr-3 align-self-start"> <!-- added align-self-start to top-align the image -->
+                                        <div class="mr-3 align-self-start">
                                             @if($lamar && $lamar->loker->perusahaan->logo)
                                         <img src="{{ asset('storage/' . $lamar->loker->perusahaan->logo) }}" alt="Logo Perusahaan" class="rounded-circle" style="width: 80px; height: 80px;">
                                         @else
@@ -61,43 +59,49 @@
                                         @endif
                                         </div>
                                         <div class="media-body">
-                                            <h5 class="media-title mb-2">
-                                                {{ $lamar->loker->judul }}
+                                            <h5 class="media-title">
+                                                <strong>{{ $lamar->loker->judul }}</strong>
                                                 <span class="badge
                                                         @if ($lamar->status === 'pending') badge-warning
                                                         @elseif ($lamar->status === 'diterima') badge-success
                                                         @elseif ($lamar->status === 'ditolak') badge-danger
                                                         @endif
-                                                    ml-2">
+                                                    ml-2" style="border-radius: 15px">
                                                     {{ $lamar->status }}
                                                 </span>
                                             </h5>
-                                            <h6 class="mb-4">{{ $lamar->loker->perusahaan->nama }}</h6> <!-- Increased spacing after the title to mb-4 for consistency -->
+                                            <h5 class="mb-4">{{ $lamar->loker->perusahaan->nama }}</h5> <!-- Increased spacing after the title to mb-4 for consistency -->
 
-                                            <div class="d-flex align-items-center justify-content-start mb-4"> <!-- mb-4 to give consistent space between rows -->
-                                                <div class="d-flex align-items-center mr-4"> <!-- Increased space between columns to mr-4 -->
-                                                    <img class="img-fluid img-icon mr-2" src="{{ asset('assets/img/landing-page/job.svg') }}"> <!-- space between icon and text -->
+                                            <div class="d-flex align-items-center justify-content-start mb-2">
+                                                <div class="d-flex align-items-center col-3"> <!-- use col-6 to take half the width -->
+                                                    <img class="img-fluid img-icon mr-2" src="{{ asset('assets/img/landing-page/job.svg') }}">
                                                     <span>{{ $lamar->loker->min_pengalaman }}</span>
                                                 </div>
-                                                <div class="d-flex align-items-center">
+                                                <div class="d-flex align-items-center col-6"> <!-- use col-6 to take half the width -->
                                                     <img class="img-fluid img-icon mr-2" src="{{ asset('assets/img/landing-page/money.svg') }}">
                                                     <span>IDR {{ $lamar->loker->gaji_bawah }} - {{ $lamar->loker->gaji_atas }}</span>
                                                 </div>
                                             </div>
-
-                                            <div class="d-flex align-items-center justify-content-start mb-4">
-                                                <div class="d-flex align-items-center mr-4"> <!-- Increased space between columns to mr-4 -->
+                                            
+                                            <div class="d-flex align-items-center justify-content-start mb-2">
+                                                <div class="d-flex align-items-left col-3"> <!-- use col-6 to take half the width -->
                                                     <img class="img-fluid img-icon mr-2" src="{{ asset('assets/img/landing-page/Graduation Cap.svg') }}">
                                                     <span>{{ $lamar->loker->min_pendidikan }}</span>
                                                 </div>
-                                                <div class="d-flex align-items-center">
+                                                <div class="d-flex align-items-center col-6"> <!-- use col-6 to take half the width -->
                                                     <img class="img-fluid img-icon mr-2" src="{{ asset('assets/img/landing-page/location pin.svg') }}">
-                                                    <span>{{ $lamar->loker->perusahaan->alamat }}</span>
+                                                    <span>{{ $lamar->loker->lokasi }}</span>
                                                 </div>
                                             </div>
                                             <small class="text-muted">
                                                 Melamar pada {{ \Carbon\Carbon::parse($lamar->created_at)->format('j F Y') }}
                                             </small>
+                                        </div>
+                                        <div class="text-center mb-3">
+                                            <a id="detail-button" class="btn btn-sm btn-primary btn-icon" style="border-radius: 25px;"
+                                                href="{{ route('all-jobs.show', $lamar->loker->id) }}">
+                                                <i class="far fa-eye"></i> Details
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -112,7 +116,6 @@
                         @endforelse
                     </div>
                 </div>
-            </div>
         </section>
     </main>
     @endsection
