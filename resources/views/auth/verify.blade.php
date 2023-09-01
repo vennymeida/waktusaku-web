de328cb1b12ee4951fd0a5b6f66aca68d54ea6fa
RosisHudaya <rosishudayaputra02@gmail.com>
2023-09-01 10:54:13 +0700

    merubah bahasa untuk verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,19 +3,19 @@
 @section('main')
     <section>
         <div class="col-md-10 mx-auto my-5 bg-white px-5 py-5 card-contact" style="border-radius: 15px;">
-            <h2 class="section-title">Please Verify Your Email</h2>
-            <div>{{ __('Verify Your Email Address') }}</div>
+            <h2 class="section-title">Harap Verifikasi Email Anda</h2>
+            <div>{{ __('Verifikasi alamat email Anda') }}</div>
             @if (session('resent'))
                 <div class="alert alert-success" role="alert">
-                    {{ __('A fresh verification link has been sent to your email address.') }}
+                    {{ __('Tautan verifikasi baru telah dikirim ke alamat email Anda.') }}
                 </div>
             @endif
 
-            {{ __('Before proceeding, please check your email for a verification link.') }}
+            {{ __('Sebelum melanjutkan, silakan periksa email Anda untuk tautan verifikasi.') }}
             <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
                 @csrf
                 <button type="submit"
-                    class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    class="btn btn-link p-0 m-0 align-baseline">{{ __('klik di sini untuk verifikasi lagi') }}</button>.
             </form>
         </div>
     </section>
