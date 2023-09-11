67c5276c9345ebdbc54932cd8f6d531e650c4673
ismafitriaa <risnandari1212@gmail.com>
2023-09-12 01:45:39 +0700

    perbaikan-seeder-pendidikan

--- database/seeders/PendidikanSeeder.php
+++ database/seeders/PendidikanSeeder.php
@@ -21,29 +21,29 @@ class PendidikanSeeder extends Seeder
             'institusi'=> "Politeknik Negeri Malang",
             'jurusan'=> "Teknik Informatika",
             'prestasi'=> "Mengikuti Lomba KMIPN",
-            'tahun_mulai'=> "2020",
+            'tahun_mulai'=> "2019",
             'tahun_berakhir'=> "2021",
-            'ipk'=> "3,45",
+            'ipk'=> "3.45",
         ]);
         Pendidikan::create([
             'user_id'=> 3,
             'gelar'=> "D3",
-            'institusi'=> "Politeknik Negeri Malang",
+            'institusi'=> "Universitas Brawijaya",
             'jurusan'=> "Teknik Informatika",
             'prestasi'=> "Mengikuti Lomba KMIPN",
-            'tahun_mulai'=> "2020",
+            'tahun_mulai'=> "2017",
             'tahun_berakhir'=> "2021",
-            'ipk'=> "3,45",
+            'ipk'=> "3.46",
         ]);
         Pendidikan::create([
             'user_id'=> 3,
-            'gelar'=> "D4",
-            'institusi'=> "Politeknik Negeri Malang",
+            'gelar'=> "S1",
+            'institusi'=> "Universitas Padjajaran",
             'jurusan'=> "Teknik Informatika",
             'prestasi'=> "Mengikuti Lomba KMIPN",
             'tahun_mulai'=> "2020",
             'tahun_berakhir'=> "2021",
-            'ipk'=> "3,45",
+            'ipk'=> "3.47",
         ]);
     }
 }
