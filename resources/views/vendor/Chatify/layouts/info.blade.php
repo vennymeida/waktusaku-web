abd66895121925930fba9bc8b8d4a7d405b5e22b
NosweL <welson160502@gmail.com>
2023-09-26 09:24:13 +0700

    latest update chatify wel

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -1,17 +1,16 @@
 {{-- user info and avatar --}}
 <div class="avatar av-l chatify-d-flex"
-@if (Auth::user()->profile && Auth::user()->profile->foto != '')
-        style="background-image: url('{{ Storage::url(Auth::user()->profile->foto) }}');"
+@if (Auth::user()->profile && Auth::user()->profile->foto != '') style="background-image: url('{{ Storage::url(Auth::user()->profile->foto) }}');"
         @else
-        style="background-image: url('{{ asset('assets/img/avatar/avatar-1.png') }}');"
-        @endif>
+        style="background-image: url('{{ asset('assets/img/avatar/avatar-1.png') }}');" @endif>
 </div>
 <p class="info-name">{{ config('chatify.name') }}</p>
 <div class="messenger-infoView-btns">
-    <a href="#" class="danger delete-conversation">Delete Conversation</a>
+    <a href="#" class="danger delete-conversation">Hapus Obrolan</a>
 </div>
 {{-- shared photos --}}
 {{-- <div class="messenger-infoView-shared">
     <p class="messenger-title"><span>Shared Photos</span></p>
     <div class="shared-photos-list"></div>
 </div> --}}
+
