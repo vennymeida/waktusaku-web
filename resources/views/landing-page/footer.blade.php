9b53ef5a74b6fb7fc71cd35b0d7f328e034e5acc
RosisHudaya <rosishudayaputra02@gmail.com>
2023-09-30 19:54:47 +0700

    mobile view footer

--- resources/views/landing-page/footer.blade.php
+++ resources/views/landing-page/footer.blade.php
@@ -4,13 +4,15 @@
         </div>
         <hr>
         <div class="row">
-            <div class="col-sm-6">
-                <img src="{{ asset('assets/img/landing-page/logo.svg') }}" alt="Logo" style="height: 40px;">
-                <span class="d-none d-sm-inline-block" style="font-size: 1rem; font-weight: 600;">Copyright © 2023
+            <div class="col-sm-6 div-footer">
+                <img class="img-footer" src="{{ asset('assets/img/landing-page/logo.svg') }}" alt="Logo"
+                    style="height: 40px;">
+                <span class="span-footer" style="font-size: 1rem; font-weight: 600;">Copyright ©
+                    2023
                     WaktuSaku.
                     All Rights Reserved.</span>
             </div>
-            <div class="col-sm-6 text-right" style="font-size: 24px">
+            <div class="col-sm-6 text-right text-footer" style="font-size: 24px">
                 <a href="#" class="text-dark"><i class="fab fa-facebook"></i></a>
                 <a href="#" class="text-dark ml-3"><i class="fab fa-instagram"></i></a>
                 <a href="#" class="text-dark ml-3"><i class="fab fa-twitter"></i></a>
